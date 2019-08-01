6989223b8fb230c571be14a1afef632bd4e19d00
fgz <1020141621@qq.com>
2019-08-01 10:47:12 +0800

    第二次

---
+++ demo1.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item,index) in lists"
+                 :key="index"
+                 @click="choose(index)"
+                 :class="{active:index==current && current!==''}"
+            >{{item}}</li>
+        </ul>
+        <div><button type="button" @click="add()">添加</button></div>
+        <ul>
+            <li v-for="(tag,i) in targets" :key="i">{{tag}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "demo1",
+        data() {
+            return {
+                lists:[1,2,3,4,5,6,7,8,9],
+                current:"",
+                targets:[]
+            }
+        },
+        methods:{
+            choose(index){
+                this.current = index;
+            },
+            add(){
+                if(this.current===''){return}
+                this.targets.push(this.lists[this.current]);
+                this.current = '';
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+li{
+    list-style: none;
+}
+li.active{
+    background: green;
+}
+</style>
